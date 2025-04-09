d1ee51ab72f8a18824ebf877784d32f8e6a648a5
HoLLds <andreyka225588@gmail.com>
2025-04-10 00:23:40 +0300

    ver1.1 deleted pages grom 'Справочник', added modal-windows instead of those pages, added JS script for modal windows, added some logos to 'Полезные ресурсы' page

---
+++ main/directory/script.js
@@ -0,0 +1,38 @@
+// Получаем все элементы кругов и модальные окна
+const circleItems = document.querySelectorAll('.circle-item');
+const modals = document.querySelectorAll('.modal');
+
+// Функция для открытия модального окна
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+}
+
+// Функция для закрытия модального окна
+function closeModal(modal) {
+    modal.style.display = 'none';
+}
+
+// Добавляем обработчики событий для кругов
+circleItems.forEach(item => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault(); // Предотвращаем переход по ссылке
+        const modalId = item.getAttribute('data-modal');
+        openModal(modalId);
+    });
+});
+
+// Добавляем обработчики событий для кнопок закрытия
+modals.forEach(modal => {
+    const closeBtn = modal.querySelector('.close');
+    closeBtn.addEventListener('click', () => {
+        closeModal(modal);
+    });
+
+    // Закрываем модальное окно при клике вне окна
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal(modal);
+        }
+    });
+});
