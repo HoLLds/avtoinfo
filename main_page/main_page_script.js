7d3f27119a52d25dd4b96a7fa4a93caa85c2a3b9
HoLLds <andreyka225588@gmail.com>
2025-04-23 20:08:02 +0300

    edits
    Co-authored-by: George <izzyjeezy@users.noreply.github.com>

--- main_page/main_page_script.js
+++ main_page/main_page_script.js
@@ -8,3 +8,41 @@ function revealElements() {
 }
 
 window.addEventListener('load', revealElements);
+
+// Все элементы списка и модальные окна
+const listItems = document.querySelectorAll('.page-text-list li');
+const modals = document.querySelectorAll('.modal');
+
+// Функция для открытия модального окна
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+}
+
+// Функция для закрытия модального окна
+function closeModal(modal) {
+    modal.style.display = 'none';
+}
+
+// Обработчики событий для элементов списка
+listItems.forEach(item => {
+    item.addEventListener('click', () => {
+        const modalId = item.getAttribute('data-modal');
+        openModal(modalId);
+    });
+});
+
+// Обработчики событий для кнопок закрытия
+modals.forEach(modal => {
+    const closeBtn = modal.querySelector('.close');
+    closeBtn.addEventListener('click', () => {
+        closeModal(modal);
+    });
+
+    // Закрытие модального окно при клике вне окна
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal(modal);
+        }
+    });
+});
